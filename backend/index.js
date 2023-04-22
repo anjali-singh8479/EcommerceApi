64f026cb0caa06e9a6f22a4766fde71bf4af44d0
anjali-singh8479 <shyanj05@gmail.com>
2023-04-22 14:45:35 +0530

    payment gateway created

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const app = express();
-const userroutes=require("../routes/user")
-const authroutes=require("../routes/auth")
-const productroutes=require("../routes/product")
-const orderroutes=require("../routes/order")
-const cartroutes=require("../routes/cart")
+const cors=require("cors")
+const userroutes=require("./routes/user")
+const authroutes=require("./routes/auth")
+const productroutes=require("../BACKEND/routes/product")
+const orderroutes=require("./routes/order")
+const cartroutes=require("../BACKEND/routes/cart")
+const striperoutes=require("../BACKEND/routes/stripe")
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 dotenv.config();
@@ -13,12 +15,15 @@ mongoose
   .connect(process.env.MONGO_URL)
   .then(() => console.log("mongo connected "))
   .catch((err) => console.log(err));
+  app.use(cors())
   app.use(express.json())
+  
   app.use("/api",userroutes)
   app.use("/api/auth",authroutes)
   app.use("/api/products",productroutes)
   app.use("/api/orders",orderroutes)
   app.use("/api/carts",cartroutes)
+  app.use("api/checkout",striperoutes)
 app.listen(5000, () => {
   console.log("backend connected");
 });
